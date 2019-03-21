b63fa3f058b37392745ba8d0a7d0e9cf6c162cd9
CIAN MC SWEENEY <ciano1000@gmail.com>
2019-03-21 14:18:16 +0000

    Oops

---
+++ src/ConwaysGame.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class ConwaysGame extends JFrame implements Runnable, KeyListener {
+
+    private boolean isInitialised =false;
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
